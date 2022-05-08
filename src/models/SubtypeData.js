23d97d811cf55397f1c082b550094c09a8ce72fe
Breno C <breno.cesa99.97@gmail.com>
2022-05-08 16:57:04 -0300

    Config Deploy to Vercel p5

--- src/models/SubtypeData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SubtypeDataSchema = new mongoose.Schema({
-  value: String,
-  name: String,
-  type: String,
-});
-
-module.exports = mongoose.model("Subtype", SubtypeDataSchema);

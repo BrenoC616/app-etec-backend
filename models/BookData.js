c73c77f384a20b22e4858427e48d41f0860d8ff2
Breno C <breno.cesa99.97@gmail.com>
2022-05-17 17:07:26 -0300

    Modify BookData Model

--- models/BookData.js
+++ models/BookData.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const BookDataSchema = new mongoose.Schema({
   coverUrl: String,
+  downloadAndReadName: String,
   title: String,
   description: String,
+  author: String,
   type: String,
   subtype: String
 });
